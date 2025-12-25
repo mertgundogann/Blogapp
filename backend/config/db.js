d07010f80cd5367f20e744a7af4cb0ead803ce4d
Mert Gündoğan <merttgundogan@hotmail.com>
2025-12-25 14:19:08 +0300

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,11 @@
-import { Client } from "pg";
+import {Client} from "pg";
 
-const client = new Client({
-  user: process.env.DB_USER,
-  password: "m+9e+2rTt",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
-});
+const db = new Client({
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    host : process.env.DB_HOST,
+    database : process.env.DB_DATABASE,
+    port : process.env.DB_PORT
+})
 
-console.log({
-  DB_USER: process.env.DB_USER,
-  USER: process.env.USER,
-  USERNAME: process.env.USERNAME,
-});
-
-client
-  .connect()
-  .then(() => console.log("PostgreSQL connected"))
-  .catch(err => console.error("DB connection error:", err));
-
-export default client;
+db.connect();
